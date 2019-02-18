ffc8b7507088e891141f202bc38cdbb29eac9238
Derrick Mei <derrick.mei.168@gmail.com>
2019-02-18 02:17:20 -0600

    derrick mei
    created template

---
+++ src/main/java/com/dkm/javacars/JavacarsApplication.java
@@ -0,0 +1,15 @@
+package com.dkm.javacars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacarsApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(JavacarsApplication.class, args);
+    }
+
+}
